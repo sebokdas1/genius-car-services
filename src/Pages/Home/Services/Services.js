1204eaf2223a70c5f9dd640e8dea29a269e5223c
sebokdas1 <sebok.das.ff@gmail.com>
2022-04-15 08:59:42 +0600

    service data loaded and show

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 
 const Services = () => {
-
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('service.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
         <div>
-            <h3>all services:</h3>
+            <h3>all services: {services.length}</h3>
+            {
+                services.map(service => <Service
+                    key={service.id}
+                    service={service}
+                />)
+            }
         </div>
     );
 };
