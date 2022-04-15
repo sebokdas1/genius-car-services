65a76f5797dc330a7bc38a7ddb2aa4c7ff88ea78
sebokdas1 <sebok.das.ff@gmail.com>
2022-04-15 09:34:06 +0600

    make simple responsive

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Service.css'
 
 const Service = ({ service }) => {
     const { name, img, description, price } = service;
     return (
-        <div>
+        <div className='service'>
             <img src={img} alt="" />
             <h2>{name}</h2>
+            <p>{price}</p>
+            <p><small>{description}</small></p>
+            <button>book now</button>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css'
 
 const Services = () => {
     const [services, setServices] = useState([]);
@@ -10,13 +11,15 @@ const Services = () => {
     }, [])
     return (
         <div>
-            <h3>all services: {services.length}</h3>
-            {
-                services.map(service => <Service
-                    key={service.id}
-                    service={service}
-                />)
-            }
+            <h1 className='service-title'>Our Services</h1>
+            <div className='service-container'>
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    />)
+                }
+            </div>
         </div>
     );
 };
